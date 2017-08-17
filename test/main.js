2646ae1a0475b0ad8487bce8a9e419a76a770890
IagoLast <iago.lastra@gmail.com>
2017-08-17 20:24:36 +0200

    Add cloudinary log

--- test/main.js
+++ test/main.js
@@ -2,6 +2,15 @@ const exquisite = require('../src/exquisite.js');
 const path = require('path');
 const expect = require('chai').expect;
 const fs = require('fs');
+const cloudinary = require('cloudinary');
+
+cloudinary.config({
+    cloud_name: 'iagolast',
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+});
+
+
 
 describe('Image comparing', () => {
     it('Should return true when the reference and the url screenshot are equal', () => {
@@ -10,6 +19,8 @@ describe('Image comparing', () => {
         const url = 'http://example.com/';
         return exquisite.test({ input, output, url }).then(actual => {
             expect(actual).to.equal(true);
+            cloudinary.uploader.upload(input, { public_id: 'reference_1' });
+            cloudinary.uploader.upload(output, { public_id: 'output_1' });
             fs.unlinkSync(output);
         });
     });
