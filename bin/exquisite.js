9a2bf4a2dd32a1002684802ef53ed6d6ccc70bfc
IagoLast <iago.lastra@gmail.com>
2017-08-19 11:29:00 +0200

    Update readme & add CLI

--- bin/exquisite.js
+++ bin/exquisite.js
@@ -1,4 +1,4 @@
-const exquisite = require('./exquisite.js');
+const exquisite = require('../src/exquisite.js');
 const argv = require('minimist')(process.argv.slice(2));
 
 exquisite.test(argv).then(console.log).catch(console.error);
