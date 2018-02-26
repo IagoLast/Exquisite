25f32efa540e36dc428bdcb794240b0d9222a4db
IagoLast <iago.lastra@gmail.com>
2018-02-26 12:33:13 +0100

    Remove unused function

--- src/exquisite.js
+++ src/exquisite.js
@@ -62,33 +62,6 @@ function getReference(args) {
     });
 }
 
-function _capture(args) {
-    let page;
-    let browser;
-    args = ArgsParser.parseArgs(args);
-    return puppeteer.launch({
-        headless: args.headless
-    }).then(function (_resp) {
-        browser = _resp;
-        return browser.newPage();
-    }).then(function (_resp) {
-        page = _resp;
-        return page.setViewport({ width: args.viewportWidth, height: args.viewportHeight });
-    }).then(function () {
-        return page.goto(args.url);
-    }).then(function () {
-        return page.waitFor(args.delay);
-    }).then(function () {
-        return page.screenshot({ path: args.output });
-    }).then(function () {
-        _closeBrowser(browser);
-        return args.output;
-    }).catch(function (err) {
-        _closeBrowser(browser);
-        throw Error(err);
-    });
-}
-
 function _closeBrowser(browser) {
     if (browser) {
         browser.close();
